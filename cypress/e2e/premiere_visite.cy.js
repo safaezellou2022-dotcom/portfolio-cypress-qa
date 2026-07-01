3e5ffa65f95f94623a7476f7ca7fd51c915b61ab
Safae Zellou <safaezellou2022@gmail.com@example.com>
2026-07-01 04:54:45 +0200

    feat: script d'interaction de recherche fonctionnel sur Boutika

--- cypress/e2e/premiere_visite.cy.js
+++ cypress/e2e/premiere_visite.cy.js
@@ -1,12 +1,19 @@
-describe('Vérification des accès plateformes', () => {
+describe('Fonctionnalité de Recherche - Boutika', () => {
 
-  // Ligne magique pour ignorer les erreurs JavaScript du site web
   Cypress.on('uncaught:exception', (err, runnable) => {
     return false
   })
      
-  it('Devrait ouvrir la page de test boutika avec succès', () => {
-    cy.visit('https://www.boutika.co.ma/')
+  it('Devrait rechercher un iPhone avec succès', () => {
+    // 1. Visiter le site
+    cy.visit('https://boutika.co.ma')
+
+    // 2. Trouver la barre de recherche avec son ID réel et écrire "iPhone"
+    cy.get('#pos_query_top').type('iPhone')
+
+    // 3. Cliquer sur le bouton de soumission du formulaire
+    //cy.get('button[type="submit"]').click()
+    cy.get('button.search_submit').click()
   })
 
 })
