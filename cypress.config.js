34fec9a689343b2876007bacff50e2d6b4a7ec24
Safae Zellou <safaezellou2022@gmail.com@example.com>
2026-07-01 15:40:34 +0200

    créer fichier produits.json et automatiser la recherche en boucle

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ module.exports = {
   allowCypressEnv: false,
 
   e2e: {
+     baseUrl: 'https://www.boutika.co.ma/', // 👈 L'URL est centralisée ici désormais
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    // implement node event listeners here
     },
   },
 };
---
+++ cypress/e2e/recherche_dynamique.cy.js
@@ -0,0 +1,39 @@
+describe('Jour 6 - Tests Data-Driven avec Fixtures sur Boutika', () => {
+
+  beforeEach(() => {
+    // Évite que le crash de la base de données de Boutika ne stoppe le test
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      return false;
+    });
+    
+    // Visite du site avec un timeout de sécurité
+    cy.visit('https://boutika.co.ma', { timeout: 10000 });
+  });
+
+  // 1. Chargement du fichier JSON contenant la liste des produits
+   it('Devrait tester la recherche pour chaque produit du catalogue', () => {
+    cy.fixture('produits').then((listeProduits) => {
+      
+      // 2. Automatisation de la recherche en boucle (ForEach)
+      listeProduits.forEach((produit) => {
+        
+        // On nettoie la barre de recherche et on tape le nom du produit courant
+        cy.get('#pos_query_top')
+          .clear()
+          .type(`${produit.nom}{enter}`);
+
+        // 3. Validation asynchrone (Le site plante sur la recherche, on valide l'URL)
+        cy.url({ timeout: 6000 }).should('include', `s=${produit.nom}`);
+        
+        // NOTE: Si le site fonctionnait, nous validerions les résultats ici.
+        // Vu le bug PrestaShop actuel, on vérifie que la page d'erreur se charge.
+        cy.contains('This page has moved', { timeout: 5000 }).should('be.visible');
+        
+         // Retour à l'accueil pour le produit suivant
+        cy.visit('/');
+      });
+      
+    });
+  });
+
+});
